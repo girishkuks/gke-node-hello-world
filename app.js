87669c578aebdd96e78700dea6961082f716d869
girishkuks <girishkuks@gmail.com>
2018-12-11 22:25:52 +0530

    Update app.js
    updated process.env.NAME

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 console.log('Hello ' + process.env.NAME)
 app.get('/', (req, res) => {
-  res.status(200).send('Hello -----------World!').end();
+  res.status(200).send('Hello -----------World!'+process.env.Name).end();
 });
 
 module.exports = app
